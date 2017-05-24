980280ad6cfa45382b8c3738f5664a8b8ec57877
yangguangyuan <172578477@qq.com>
2017-05-24 14:05:31 +0800

    123123

--- web/c#main.js
+++ web/c#main.js
@@ -1,6 +1,6 @@
 (function ($) {
 
-    //登录相关
+    //跳转去登录
     $.toSignin = function (back) {
         var from = back || (location.pathname + location.search);
 
@@ -10,41 +10,45 @@
 
             window.location.replace(window.URL + 'signin.html' + (from ? ('?from=' + $.base64encode(from)) : ''))
         }
-    }
+    };
 
+    //往ls里面放入用户信息
     $.putUser = function (rs) {
         localStorage['yes'] = $.base64encode(JSON.stringify(rs))
-    }
+    };
+    //不传参就是获取用户信息对象 传参获取单个用户信息
     $.getUser = function (rs) {
         return rs ? JSON.parse($.base64decode(localStorage['yes']))[rs] : JSON.parse($.base64decode(localStorage['yes']))
-    }
+    };
+    //清楚ls
     $.clearUser = function () {
         localStorage.clear()
-    }
+    };
+    //更新用户信息
     $.editUser = function (res, val) {
-        var data = JSON.parse($.base64decode(localStorage['yes']))
-        data[res] = val
+        var data = JSON.parse($.base64decode(localStorage['yes']));
+        data[res] = val;
         $.putUser(data)
-    }
-
+    };
+    //保存LStoken
     $.putToken = function (res) {
         localStorage['yesToken'] = $.base64encode(res.PhoneNumber + ':' + res.DynamicToken);
-    }
-
+    };
+    //检查是否登录，并且拼接head
     $.checkUser = function () {
 
         if ($.is_weixin()) {
-            var user = Cookies.getJSON('userInfo')
+            var user = Cookies.getJSON('userInfo');
             if (user) {
-                user = JSON.parse($.base64decodes(user))
-                $.putUser(user)
-                $.putToken(user)
+                user = JSON.parse($.base64decodes(user));
+                $.putUser(user);
+                $.putToken(user);
                 Cookies.remove('userInfo', {path: '/'});
             }
 
         }
 
-        window.TOKEN = localStorage['yesToken']
+        window.TOKEN = localStorage['yesToken'];
         if (window.TOKEN) {
             $.ajaxSetup({
                 headers: {
@@ -54,14 +58,15 @@
         } else {
             $.toSignin()
         }
-    }
+    };
 
+    //倒计时
     $.CountDown = function (obj) {
         var t = 60;
         var timer = setInterval(function () {
             if (t == 0) {
                 obj.html('获取验证码');
-                obj.removeClass('on')
+                obj.removeClass('on');
                 clearInterval(timer);
             } else {
                 obj.html(t + '秒后重发');
@@ -69,12 +74,13 @@
             }
 
         }, 1000)
-    }
+    };
 
+    //根据接口返回 显示错误信息 或者 返回授权
     $(document).ajaxSuccess(function (event, xhr, settings) {
 
         if (xhr.responseText) {
-            var res = JSON.parse(xhr.responseText)
+            var res = JSON.parse(xhr.responseText);
             if (res.returnCode == '401') {
                 $.toSignin()
             }
@@ -88,4 +94,4 @@
     });
 
 
-})(jQuery)
+})(jQuery);
